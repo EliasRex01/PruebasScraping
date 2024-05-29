e21b5acbb449ac20b4042e9ddab1c1ec747f3273
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-29 18:38:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -28,6 +28,9 @@ options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1080')
 
 # Ruta específica de Chrome y ChromeDriver
 options.binary_location = '/usr/bin/google-chrome'
@@ -44,7 +47,7 @@ def action(path='', value='click'):
             if '/' in path:
                 element = driver.find_element(By.XPATH, path)
             else:
-                element = driver.find_element(By.ID, path)        
+                element = driver.find_element(By.ID, path)
             if value == 'click':
                 element.click()
             else:
