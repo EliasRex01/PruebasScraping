4e23a2594d5517472d994fb3653c1c4ba99ff976
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-30 13:26:29 -0400

    Create basico.py

---
+++ basico.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# Configurar las opciones de ChromeDriver
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1080')
+
+# Ruta específica de Chrome y ChromeDriver
+options.binary_location = '/usr/bin/google-chrome'
+chrome_service = Service('/usr/local/bin/chromedriver', log_path='/path/to/chromedriver.log')  # Cambia '/path/to/chromedriver.log' a la ruta deseada para el archivo de log
+
+# Inicializar el WebDriver
+driver = webdriver.Chrome(service=chrome_service, options=options)
+
+# Abrir una página y verificar el título
+driver.get('http://www.google.com')
+print(driver.title)
+
+# Cerrar el WebDriver
+driver.quit()
