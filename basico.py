3b6ab10e8f7563caacc1acdecc71026b3f02ae15
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-05-30 14:59:13 -0400

    Update basico.py

--- basico.py
+++ basico.py
@@ -8,11 +8,14 @@ options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.ignore_local_proxy_environment_variables()  # Mover esta configuración a BaseOptions
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-background-networking')
+options.ignore_local_proxy_environment_variables()
 
 # Ruta específica de Chrome y ChromeDriver
 options.binary_location = '/usr/bin/google-chrome'
-chrome_service = Service('/usr/bin/chromedriver')
+chrome_service = Service('/usr/local/bin/chromedriver')
 
 # Inicializar el WebDriver
 driver = webdriver.Chrome(service=chrome_service, options=options)
