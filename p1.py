6b1df6f4fda44183f034cee9da5c4de99f7b1dad
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2024-06-01 18:23:17 -0400

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,42 +1,163 @@
-from playwright.sync_api import sync_playwright
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style, init
+import time
+import requests
+import platform
+import json
+import sys
+import os
+import shutil
+from datetime import datetime
 
-def buscar_en_google(term):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        
-        # Navegar a Google
-        page.goto('https://www.google.com')
+# Inicializar colorama
+init(autoreset=True)
+fecha_actual = datetime.now().strftime('%d/%m/%Y')
+tiempo_inicio = time.time()
+sistema_operativo = platform.system()
 
-        # palabra de busqueda y presionar Enter
-        page.fill('input[name="q"]', term)
-        page.press('input[name="q"]', 'Enter')
+# FUNCION WAIT TRUE
+def action(path='', value='click'):
+    while True:
+        try:
+            if '/' in path:
+                element = driver.find_element(By.XPATH, path)
+            else:
+                element = driver.find_element(By.ID, path)        
+            if value == 'click':
+                element.click()
+            else:
+                element.send_keys(value)
+        except Exception as x:
+            time.sleep(1)
+            continue
+        break
 
-        # Esperar a que aparezcan los resultados
-        page.wait_for_selector('div#search')
+# CARGAR EL NUMERO DE JUICIO
+juicio = input(Back.WHITE + Fore.RED + "Ingresa Nro de juicio: ")
+print(Back.WHITE + Fore.BLUE + "🚀 Iniciar proceso !!")
 
-        # Obtener la cantidad de resultados
-        resultados = page.query_selector_all('div.g')
-        cantidad_resultados = len(resultados)
+url = 'https://unionnegocios.com.py/sistema/juicios/datos/' + juicio
+try:
+    headers = {"Accept": "application/json"} # Cambiar Content-Type a Accept
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json() # data['id_juicios']
+        cedula = data['ci1']
+        monto = data['monto']
+        print(Back.WHITE + Fore.BLACK + "--- CI: " + " | DEM: " + data['dem1'] + data['ci1'] + " | MONTO: " + data['monto'] )
+    else:
+        print(f"\x1b[⚠️ Error.... {response.status_code}\x1b[0m")
+        sys.exit()
+except Exception as e:
+    print(f"Error: {str(e)}")
+    sys.exit()
 
-        # Cerrar el navegador
-        browser.close()
+print(Back.WHITE + Fore.BLUE + "⌛ procesando....")
 
-        return cantidad_resultados
+# URL del sitio web que deseas procesar
+url = 'https://ingresosjudiciales.csj.gov.py/LiquidacionesWeb/loginAbogados.seam'
 
-if __name__ == "__main__":
-    termino_busqueda = "ardillas"
-    cantidad_resultados = buscar_en_google(termino_busqueda)
-    print(f"Cantidad de resultados para '{termino_busqueda}': {cantidad_resultados}")
+# Configurar el navegador Chrome para que se ejecute en modo headless
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--window-size=1200x1000')
 
-# bibliotecas necesarias ejecutando `pip install playwright`. 
-# se abre una ventana del navegador (cambiar `headless=True` a `headless=False`)
+# Configurar la ruta de descarga predeterminada
+prefs = {"download.default_directory": "/root/descargas"}
+options.add_experimental_option("prefs", prefs)
 
+driver = webdriver.Chrome(options=options)
+driver.get(url)
 
+wait = WebDriverWait(driver, 10)
 
+action('j_id3:username', '1591666')
+action('j_id3:password', 'estudioAmarillaCloss2')
+action('j_id3:submit', 'click')
+action('iconabogadosFormId:j_id17', 'click')
+action('iconabogadosFormId:j_id18', 'click')
+action('juicioFormId:fechaIdInputDate', fecha_actual)
 
+# Agregar Demandante
+wait.until(EC.element_to_be_clickable((By.ID, 'juicioFormId:j_id59'))).click() 
+tipo_doc_demandante = Select(wait.until(EC.element_to_be_clickable((By.ID, 'juicioFormId:demandantesListId:0:tipoDocumentoContribuyenteId'))))
+tipo_doc_demandante.select_by_value('1')
+time.sleep(1)
+nro_doc_demandante = driver.find_element(By.ID, 'juicioFormId:demandantesListId:0:numeroDocumentoContribuyenteId')
+nro_doc_demandante.send_keys('80111738-0')
 
+# Agregar demandado
+wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/span/table[1]/tbody/tr[6]/td/table/tbody/tr/td[2]/a'))).click()
+nro_doc_demandado = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/span/table[1]/tbody/tr[7]/td/table/tbody/tr/td[2]/input')))
+nro_doc_demandado.send_keys(cedula)
 
+if data['ci2'] is not None and data['ci2'].isdigit():
+    wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/span/table[1]/tbody/tr[6]/td/table/tbody/tr/td[2]/a'))).click()
+    nro_doc_demandado2 = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/span/table[1]/tbody/tr[7]/td/table/tbody/tr[2]/td[2]/input')))
+    nro_doc_demandado2.send_keys(data['ci2'])
 
+agregarConcepto = wait.until(EC.element_to_be_clickable((By.ID, 'juicioFormId:j_id109'))).click()
 
-C
+# Hacer clic en el elemento 'agregarAccionPrep'
+agregarAccionPrep = wait.until(EC.element_to_be_clickable((By.NAME, 'modalPanelFormId:conceptosListId:5:j_id196'))).click()
+agregarMonto = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[2]/table/tbody/tr/td/form/span/div/div/div[2]/table/tbody/tr/td/div/table/tbody/tr[1]/td[5]/div/input')))
+agregarMonto.send_keys(monto)
+time.sleep(1)
+grabar = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div[2]/div/div[2]/table/tbody/tr/td/form/table/tbody/tr/td[1]/input'))).click()
+
+# Espera a que aparezca el cuadro de diálogo
+grabar2 = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/table/tbody/tr/td/input'))).click()
+time.sleep(1)
+alert = WebDriverWait(driver, 3).until(EC.alert_is_present())
+alert.accept()
+print(Back.WHITE + Fore.BLUE + "⚠️ Formulario aceptado")
+
+# Para finalizar se tiene que descargar la tasa judicial
+time.sleep(1)
+imprimir = driver.find_element(By.XPATH, '/html/body/table/tbody/tr[5]/td/div/div/table/tbody/tr/td[2]/table/tbody/tr[6]/td/table/tbody/tr[2]/td[1]/table/tbody/tr/td/form/table/tbody/tr/td[1]/input').click()
+
+# Ruta de la carpeta de descargas
+carpeta_descargas = "/root/descargas"
+
+# Obtener una lista de archivos en la carpeta de descargas
+archivos_en_descargas = os.listdir(carpeta_descargas)
+# Inicializa la variable para el número más alto
+numero_mas_alto = 0
+# Busca el número más alto en los nombres de los archivos
+for archivo in archivos_en_descargas:
+    if archivo.startswith("liquidacionJuicio") and archivo.endswith(".pdf"):
+        # Extrae solo los dígitos del nombre del archivo
+        numero_str = ''.join(filter(str.isdigit, archivo))
+        if numero_str:
+            numero = int(numero_str)
+            numero_mas_alto = max(numero_mas_alto, numero)
+
+# Construye el nombre del archivo más alto
+nombre_archivo_mas_alto = f"liquidacionJuicio{numero_mas_alto}.pdf"
+# Rutas de archivo de origen y carpeta de destino
+archivo_a_copiar = os.path.join(carpeta_descargas, nombre_archivo_mas_alto)
+path_carpeta_destino = "/root/descargas"
+carpeta_destino = os.path.join(path_carpeta_destino, f"{juicio}-tasa.pdf")
+
+# Copia el archivo a la carpeta de destino y cambia su nombre
+shutil.copy(archivo_a_copiar, carpeta_destino)
+
+# Ruta del archivo original en la carpeta de descargas
+archivo_original = os.path.join(carpeta_descargas, nombre_archivo_mas_alto)
+
+# Borra el archivo original en la carpeta de descargas
+os.remove(archivo_original)
+
+time.sleep(1)
+tiempo_fin = time.time()
+duracion = round(tiempo_fin - tiempo_inicio)
+print(Back.WHITE + Fore.BLUE + "✅ 🎉 FINALIZADO. En " + str(duracion) + " segundos !!!")
+driver.quit()
